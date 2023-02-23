3953c9ae3992c129453bb02e5e953c1f30fcb207
Rdoolz51 <Rdoolz51@aol.com>
2023-02-23 02:18:50 +0000

    throwing unchecked exception

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,9 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
+    int a = 0;
+    int sum;
     public void throwUncheckedException(){
-
+        sum = 4 / a;
     }
 }
